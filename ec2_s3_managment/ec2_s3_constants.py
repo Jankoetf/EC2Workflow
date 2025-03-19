9595727718f7ff80cebb9b723ac9742f6f12a5a0
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-20 00:07:31 +0100

    added s3 and ec2 constants

---
+++ ec2_s3_managment/ec2_s3_constants.py
@@ -0,0 +1,7 @@
+EC2_KEY_NAME = "jankoioi"
+KEY_DIR = "ec2_managment"
+INSTANCE_AMI = "ami-08b5b3a93ed654d19"
+
+SECURITY_GROUP_NAME = "jankoioi_security"
+INSTANCE_NAME = "test_workflow"
+DESCIPTION = "SSH"
