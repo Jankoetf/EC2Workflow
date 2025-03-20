1d80b67c91d5a8f933df2cd40ea82ebb3783a84a
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-20 16:20:24 +0100

    local and cloud pipeline tested

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def run_local_training_pipeline():
     model_instance.train_model(X_train, y_train)
 
     #evaluate model
-    metrics = model_instance.evaluate_model(X_test, y_test)
+    metrics = ModelClass.evaluate_model_static(model_instance.model, X_test, y_test)
 
     # upload metrics, model, logger to S3
     s3_manager_instance.upload_metrics_to_s3(metrics)
@@ -47,6 +47,49 @@ def run_local_training_pipeline():
     
     logger.info(f"=== Logger END ===\n\n")
 
+def run_cloud_training_pipeline():
+    """Main function to execute the full training pipeline."""
+    logger.info(f"=== Starting model training pipeline ===")
+    s3_manager_instance = S3ManagerClass()
+    loader_instance = LoaderClass()
+    model_instance = ModelClass()
+
+    # Load data
+    X_train, y_train, X_test, y_test = loader_instance.load_training_data()
+
+    # Train model
+    model_instance.train_model(X_train, y_train)
+
+    #evaluate model
+    metrics = ModelClass.evaluate_model_static(model_instance.model, X_test, y_test)
+
+    # upload metrics, model, logger to S3
+    s3_manager_instance.upload_metrics_to_s3(metrics)
+    s3_manager_instance.upload_model_to_s3(model_instance.model)
+    logger.info("=== Model training pipeline completed ===")
+    s3_manager_instance.upload_log_to_s3()
+
+    logger.info(f"=== Logger END ===\n\n")
+
+def download_experiment_data_and_evaluate_model():
+    s3_manager_instance = S3ManagerClass()
+    loader_instance = LoaderClass()
+    
+    # Load train test data
+    X_train, y_train, X_test, y_test = loader_instance.load_training_data()
+
+    # download metrics, model, logger from S3
+    s3_manager_instance.download_experiment_files_from_s3()
+
+    # load model
+    loaded_model = s3_manager_instance.load_model_localy()
+
+    # evaluate model
+    metrics = ModelClass.evaluate_model_static(loaded_model, X_test, y_test)
+    print("accuracy: ", metrics["accuracy"])
+
+
 if __name__ == "__main__":
     print("main")
-    run_local_training_pipeline()
+    # run_cloud_training_pipeline()
+    download_experiment_data_and_evaluate_model()
