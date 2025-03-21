e92c61fbbe7c66b186d5b2cc2158de38889b6ae7
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-21 20:05:53 +0100

    added role name

--- ec2_s3_managment/ec2_s3_constants.py
+++ ec2_s3_managment/ec2_s3_constants.py
@@ -3,9 +3,12 @@ EC2_KEY_NAME = "jankoioi"
 KEY_DIR = "ec2_s3_managment"
 INSTANCE_AMI = "ami-08b5b3a93ed654d19"
 
-SECURITY_GROUP_NAME = "jankoioi_security"
+SECURITY_GROUP_NAME = "jankoioi_security_2"
 INSTANCE_NAME = "test_workflow"
 DESCIPTION = "SSH"
+
+#IAM
+ROLE_NAME = "ec2-s3-full-access"
 
 #S3 constants
 METRICS_FILENAME = "metrics"
