9e4fbcb1c5dc5df06068350c1a6ce2e7c0dba737
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-23 19:10:42 +0100

    commented - added explanation for shut down

--- ec2_s3_managment/user_data.py
+++ ec2_s3_managment/user_data.py
@@ -1,3 +1,18 @@
+"""
+Docker Execution Script
+
+This script defines the EC2 instance's user data, which runs automatically when the instance starts.
+
+Key points:
+    sleep 60
+        Instance waits for 60 seconds because instance will first start running and only then S3 access rule will
+        be attached - so we need to wait until EC2 instance has S3 access
+    shutdown -h now
+        Initiates system shutdown once the container execution completes,
+        Combined with instance 'terminate on shutdown' setting, this ensures complete cleanup
+        All resources (instance, EBS volumes) are automatically terminated and deleted
+"""
+
 IMAGE_NAME = "jankoi/model-training:latest"
 user_data = f"""#!/bin/bash
 yum update -y
