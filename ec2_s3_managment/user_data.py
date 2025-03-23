494cb6a6bcf3ac3c9907c84ea2f1e96d0f4e8329
Jankoetf <jankomitrovicetf@gmail.com>
2025-03-23 13:50:02 +0100

    image name is a variable

--- ec2_s3_managment/user_data.py
+++ ec2_s3_managment/user_data.py
@@ -1,10 +1,11 @@
-user_data = """#!/bin/bash
+IMAGE_NAME = "jankoi/model-training:latest"
+user_data = f"""#!/bin/bash
 yum update -y
 yum install docker -y
 systemctl start docker
 systemctl enable docker
-docker pull jankoi/model-training:latest
+docker pull {IMAGE_NAME}
 sleep 60
-docker run jankoi/model-training:latest
+docker run {IMAGE_NAME}
 shutdown -h now
 """
